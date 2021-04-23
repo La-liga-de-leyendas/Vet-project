fe479d7c01744bdb4feffd803e4f5b2a182e87ea
Luz Mariana Carlo Huasco <luzmarianacarlo1999@gmail.com>
2021-04-23 13:33:59 -0400

    register page

--- vet-patitas/src/app/register/register.module.ts
+++ vet-patitas/src/app/register/register.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const routes: Routes = [
   {path: '', component: RegisterComponent }
@@ -12,7 +18,13 @@ const routes: Routes = [
     RegisterComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forChild(routes)
   ],
   exports: [
