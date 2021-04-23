ee67faae6dc2b3822e8ce3fe0226338431aa6861
SaraUP <sarisup14@gmail.com>
2021-04-23 12:55:05 -0400

    Primer card

--- vet-patitas/src/app/pages/products/products.module.ts
+++ vet-patitas/src/app/pages/products/products.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 const routes: Routes = [
   {path: '', component: ProductsComponent}
@@ -13,6 +16,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
     RouterModule.forChild(routes)
   ]
 })
