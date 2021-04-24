19fc8b0dc977a91f1f5dbd9cb14643bb07ff325a
Luz Mariana Carlo Huasco <luzmarianacarlo1999@gmail.com>
2021-04-23 21:28:45 -0400

    profile nabvar

--- vet-patitas/src/app/pages/pages-routing.module.ts
+++ vet-patitas/src/app/pages/pages-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
       {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
       {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
       {path: 'vet-services', loadChildren: () => import('./vet-services/vet-services.module').then(m => m.VetServicesModule)},
-      {path: 'contact-us', loadChildren: () => import('./contact-us/contact-us.module').then(m => m.ContactUsModule)}
+      {path: 'contact-us', loadChildren: () => import('./contact-us/contact-us.module').then(m => m.ContactUsModule)},
+      {path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)}
     ]
   }
 ];
