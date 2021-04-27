bc3abb49fb65f97987dbc589f7a0245c6de76e74
Luz Mariana Carlo Huasco <luzmarianacarlo1999@gmail.com>
2021-04-26 20:02:45 -0400

    login, environment prod, and pages

--- vet-patitas/src/environments/environment.prod.ts
+++ vet-patitas/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAUFrEAkZgcxUpviLlbPKx68zaTy6iOa2w",
+    authDomain: "vet-website-caa3e.firebaseapp.com",
+    databaseURL: "https://vet-website-caa3e-default-rtdb.firebaseio.com",
+    projectId: "vet-website-caa3e",
+  },
+  app: {
+    apiBaseUrl: 'https://vet-website-caa3e-default-rtdb.firebaseio.com'
+  },
+  auth: {
+    apiBaseUrl: 'https://identitytoolkit.googleapis.com',
+    key: 'AIzaSyAUFrEAkZgcxUpviLlbPKx68zaTy6iOa2w'
+  }
 };
