82f039fd0fa9614922c016f657e35271d4668712
fer martinez <f.l.m.m.0027@gmail.com>
2021-05-06 15:28:46 -0400

    mensaje de usuario no registrado en español

--- vet-patitas/src/app/shared/services/registerauth.service.ts
+++ vet-patitas/src/app/shared/services/registerauth.service.ts
@@ -41,7 +41,7 @@ export class RegisterauthService {
         });
         this.SetUserData(result.user);
       }).catch((error) => {
-        window.alert('Parece que este usuario no existe.')
+        window.alert('Parece que esta cuenta no está registrada, verifica las credenciales.')
       })
   }
 
