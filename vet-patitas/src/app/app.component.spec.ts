988a6a322790ac64948d340ea2b3cc53da580677
Luz Mariana Carlo Huasco <luzmarianacarlo1999@gmail.com>
2021-05-12 15:52:09 -0400

    prueba

--- vet-patitas/src/app/app.component.spec.ts
+++ vet-patitas/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [RouterTestingModule]
     }).compileComponents();
   });
 
